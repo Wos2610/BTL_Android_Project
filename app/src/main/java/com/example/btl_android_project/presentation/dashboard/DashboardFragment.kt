37e21f1c9583bdd422ade978eac6138b78dfb213
Wos2610 <nt2nguyenthingocthuy@gmail.com>
2025-03-19 01:12:54 +0700

    [T012] Navigation

--- app/src/main/java/com/example/btl_android_project/presentation/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/btl_android_project/presentation/dashboard/DashboardFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.btl_android_project.R
 import com.example.btl_android_project.databinding.FragmentDashboardBinding
 
@@ -29,6 +32,11 @@ class DashboardFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.tvFood.setOnClickListener{
+            val action = DashboardFragmentDirections.actionDashboardFragmentToLogMealFragment()
+            findNavController().navigate(action)
+        }
     }
 
     override fun onDestroyView() {
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,14 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("androidx.navigation.safeargs") version "2.8.9" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+    }
 }
